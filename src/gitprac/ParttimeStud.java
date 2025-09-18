016e6b2ee4662983f3e16c742d89df684d57a24f
EjazRay <raymane@sheridancollege.ca>
2025-09-18 17:31:30 -0400

    added code

---
+++ src/gitprac/ParttimeStud.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gitprac;
+
+/**
+ *
+ * @author ejazr
+ */
+public class ParttimeStud {
+    private int sid;
+    private int numofHours; 
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    /**
+     * @return the numofHours
+     */
+    public int getNumofHours() {
+        return numofHours;
+    }
+
+    /**
+     * @param numofHours the numofHours to set
+     */
+    public void setNumofHours(int numofHours) {
+        this.numofHours = numofHours;
+    }
+}
